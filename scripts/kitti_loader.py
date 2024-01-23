9ad55803facf91b46aa5ba965b6b24adca3673e5
mhmd-j <mo.jani1378@gmail.com>
2024-01-22 19:22:08 -0800

    kitti data loader for yolo det added, without gt yet

--- scripts/kitti_loader.py
+++ scripts/kitti_loader.py
@@ -24,177 +24,173 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from collections import namedtuple
+
 
 from ultralytics.yolo.data.augment import LetterBox
 from ultralytics.yolo.data.utils import IMG_FORMATS, VID_FORMATS
 from ultralytics.yolo.utils import LOGGER, ROOT, is_colab, is_kaggle, ops
 from ultralytics.yolo.utils.checks import check_requirements
 import threading
+import kitti_loader_utils as utils
+class KittiLoader:
+    def __init__(self, kitii_base_path: str, sequence: str, transform = None, 
+                    focalx = 320.0, focaly = 320.0, centerx = 320.0, centery = 240.0):
+            self.base_path = kitii_base_path
+            self.sequence = sequence
 
-class LoadStreams:
-    # YOLOv8 streamloader, i.e. `python detect.py --source 'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP streams`
-    def __init__(self, sources='file.streams', imgsz=640, stride=32, auto=True, transforms=None, vid_stride=1, time_out = 2):
-        is_ros = isinstance(sources, image_converter)
-        torch.backends.cudnn.benchmark = True  # faster for fixed-size inference
-        self.mode = 'stream'
-        self.imgsz = imgsz
-        self.stride = stride
-        self.vid_stride = vid_stride  # video frame-rate stride
-        self.depth_image = None
-        self.time_out = time_out
-        self.lock = threading.Lock()
-        self.lock_extra = threading.Lock()
-        if not is_ros:
-            sources = Path(sources).read_text().rsplit() if os.path.isfile(sources) else [sources]
-        else:
-            sources = [sources]
-        n = len(sources)
-        if not is_ros:
-            self.sources = [ops.clean_str(x) for x in sources]  # clean source names for later
-        else:
-            self.sources = sources
-        self.imgs, self.fps, self.frames, self.threads = [None] * n, [0] * n, [0] * n, [None] * n
-        for i, s in enumerate(sources):  # index, source
-            # Start thread to read frames from video stream
-            st = f'{i + 1}/{n}: {s}... '
-            if not is_ros:
-                if urlparse(s).hostname in ('www.youtube.com', 'youtube.com', 'youtu.be'):  # if source is YouTube video
-                    # YouTube format i.e. 'https://www.youtube.com/watch?v=Zgi9g1ksQHc' or 'https://youtu.be/Zgi9g1ksQHc'
-                    check_requirements(('pafy', 'youtube_dl==2020.12.2'))
-                    import pafy  # noqa
-                    s = pafy.new(s).getbest(preftype="mp4").url  # YouTube URL
-                s = eval(s) if s.isnumeric() else s  # i.e. s = '0' local webcam
-                if s == 0:
-                    assert not is_colab(), '--source 0 webcam unsupported on Colab. Rerun command in a local environment.'
-                    assert not is_kaggle(), '--source 0 webcam unsupported on Kaggle. Rerun command in a local environment.'
-                # if "rtsp" in s:
-
-                cap = cv2.VideoCapture(s)
-                assert cap.isOpened(), f'{st}Failed to open {s}'
-                w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                fps = cap.get(cv2.CAP_PROP_FPS)  # warning: may return 0 or nan
-                self.frames[i] = max(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), 0) or float('inf')  # infinite stream fallback
-                self.fps[i] = max((fps if math.isfinite(fps) else 0) % 100, 0) or 30  # 30 FPS fallback
-
-                _, self.imgs[i] = cap.read()  # guarantee first frame
-            else:
-                # while s.cv_image_queue.qsize() == 0: pass
-                # if s.cv_image_queue.qsize() > 0:
-                #     while(s.cv_image_queue.qsize() > 0):
-                #         self.imgs[i] = s.cv_image_queue.get()
-                self.imgs[i] = s.cv_image_queue.get()
-                w = int(self.imgs[i].shape[0])
-                h = int(self.imgs[i].shape[1])
-            if not is_ros:
-                self.threads[i] = Thread(target=self.update, args=([i, cap, s]), daemon=True)
-            else:
-                self.threads[i] = Thread(target=self.update, args=([i, None, s]), daemon=True)
-            LOGGER.info(f"{st} Success ({self.frames[i]} frames {w}x{h} at {self.fps[i]:.2f} FPS)")
-            self.threads[i].start()
-        LOGGER.info('')  # newline
-
-        # check for common shapes
-        s = np.stack([LetterBox(imgsz, auto, stride=stride)(image=x).shape for x in self.imgs])
-        self.rect = np.unique(s, axis=0).shape[0] == 1  # rect inference if all shapes equal
-        self.auto = auto and self.rect
-        self.transforms = transforms  # optional
-        if not self.rect:
-            LOGGER.warning('WARNING ⚠️ Stream shapes differ. For optimal performance supply similarly-shaped streams.')
-
-    def update(self, i, cap, stream):
-        # Read stream `i` frames in daemon thread
+            # Find all the data files
+            self._get_file_lists()
+            self._load_calib()
+            self._load_oxts() # it loads all the oxts data for a sequence. not an effiecient way to load oxts data
+            self.transform = transform
+    
+    def _get_file_lists(self):
+        """Find and list data files for each sensor."""
+        self.cam2_files = sorted(glob.glob(
+            os.path.join(self.base_path,
+                         'image_02',
+                         self.sequence,
+                         '*.{}'.format(self.imtype))))
+        self.cam3_files = sorted(glob.glob(
+            os.path.join(self.base_path,
+                         'image_03',
+                         self.sequence,
+                         '*.{}'.format(self.imtype))))
+        self.velo_files = sorted(glob.glob(
+            os.path.join(self.base_path,
+                        'velodyne',
+                        self.sequence,
+                         '*.bin')))
         
-        is_ros = isinstance(stream, image_converter)
-        if is_ros:
-            while True:            
-                my_im = stream.cv_image_queue.get()
-                my_im = cv2.resize(my_im, (0,0), fx=1., fy=1.)
-                im = my_im
-                self.imgs[i] = im
-                if self.lock_extra.locked():
-                    self.lock_extra.release()
-                self.lock.acquire()
-                #check sim_reset_queue size
-                
+        self.oxts_files = sorted(glob.glob(
+            os.path.join(self.base_path,
+                        'oxts',
+                         f'{self.sequence}.txt')))
+    def _load_calib(self):
+        """Load and compute intrinsic and extrinsic calibration parameters."""
+        # We'll build the calibration parameters as a dictionary, then
+        # convert it to a namedtuple to prevent it from being modified later
+        data = {}
+
+        # Load the calibration file
+        calib_filepath = os.path.join(self.base_path,'calib', f'{self.sequence}.txt')
+        filedata = utils.read_calib_file(calib_filepath)
+
+        # Create 3x4 projection matrices
+        P_rect_00 = np.reshape(filedata['P0'], (3, 4))
+        P_rect_10 = np.reshape(filedata['P1'], (3, 4))
+        P_rect_20 = np.reshape(filedata['P2'], (3, 4))
+        P_rect_30 = np.reshape(filedata['P3'], (3, 4))
+
+        data['P_rect_00'] = P_rect_00
+        data['P_rect_10'] = P_rect_10
+        data['P_rect_20'] = P_rect_20
+        data['P_rect_30'] = P_rect_30
+
+        # Compute the rectified extrinsics from cam0 to camN
+        T1 = np.eye(4)
+        T1[0, 3] = P_rect_10[0, 3] / P_rect_10[0, 0]
+        T2 = np.eye(4)
+        T2[0, 3] = P_rect_20[0, 3] / P_rect_20[0, 0]
+        T3 = np.eye(4)
+        T3[0, 3] = P_rect_30[0, 3] / P_rect_30[0, 0]
+
+        Tr_velo_cam = np.reshape(filedata['Tr_velo_cam'], (3, 4))
+        data['Tr_velo_cam'] = np.vstack([Tr_velo_cam, [0, 0, 0, 1]])
+
+        Tr_imu_velo = np.reshape(filedata['Tr_imu_velo'], (3, 4))
+        data['Tr_imu_velo'] = np.vstack([Tr_imu_velo, [0, 0, 0, 1]])
+
+        # Compute the camera intrinsics
+        data['K_cam0'] = P_rect_00[0:3, 0:3]
+        data['K_cam1'] = P_rect_10[0:3, 0:3]
+        data['K_cam2'] = P_rect_20[0:3, 0:3]
+        data['K_cam3'] = P_rect_30[0:3, 0:3]
+
+        self._calib = namedtuple('CalibData', data.keys())(*data.values())
+    
+    @property
+    def calib(self):
+        """Return a namedtuple of calibration parameters."""
+        return self._calib
+    
+    
+    
+    def _load_oxts(self):
+        """Load OXTS data from file."""
+        self.oxts = utils.load_oxts_packets_and_poses(self.oxts_files)
+        """Generator to read OXTS ground truth data.
+
+           Poses are given in an East-North-Up coordinate system 
+           whose origin is the first GPS position.
+        """
+        # Scale for Mercator projection (from first lat value)
+        scale = None
+        # Origin of the global coordinate system (first GPS position)
+        origin = None
+
+        oxts = []
+
+        for filename in self.oxts_files:
+            with open(filename, 'r') as f:
+                for line in f.readlines():
+                    line = line.split()
+                    # Last five entries are flags and counts
+                    line[:-5] = [float(x) for x in line[:-5]]
+                    line[-5:] = [int(float(x)) for x in line[-5:]]
+
+                    packet = utils.OxtsPacket(*line)
+
+                    if scale is None:
+                        scale = np.cos(packet.lat * np.pi / 180.)
+
+                    R, t = utils.pose_from_oxts_packet(packet, scale)
+
+                    if origin is None:
+                        origin = t
+
+                    T_w_imu = utils.transform_from_rot_trans(R, t - origin)
+
+                    oxts.append(utils.OxtsData(packet, T_w_imu))
+
+        self.oxts = oxts
+            
+    def __getitem__(self, index):
+        """Return the data from a particular index."""
+        # Load the data from disk
+        cam2_0 = cv2.imread(self.cam2_files[index].strip())
+        velo = np.fromfile(self.velo_files[index].strip(), dtype=np.float32)
+        oxt = self.oxts[index]
+
+        # Apply the data transformations
+        if self.transform is not None:
+            cam2 = self.transform(cam2)
         else:
-            n, f = 0, self.frames[i]  # frame number, frame array
-            while cap.isOpened() and n < f:
-                n += 1
-                cap.grab()  # .read() = .grab() followed by .retrieve()
-                if n % self.vid_stride == 0:
-                    success, im = cap.retrieve()
-                    print("In update loop of stream_loader  ", success)
-                    if success:
-                        self.imgs[i] = im
-                    else:
-                        LOGGER.warning('WARNING ⚠️ Video stream unresponsive, please check your IP camera connection.')
-                        self.imgs[i] = np.zeros_like(self.imgs[i])
-                        cap.open(stream)  # re-open stream if signal was lost
-        time.sleep(0.0)  # wait time
+            cam2 = LetterBox(self.imgsz, self.auto, stride=self.stride)(image=cam2_0)
+            cam2 = cam2_0.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+            cam2 = np.ascontiguousarray(cam2_0)  # contiguous
+        
+        self.extra_output = { "velodyne": velo, "oxt": oxt, "gt": self.gt} #TODO: add gt
 
-    def __iter__(self):
-        self.count = -1
-        return self
-
+        return self.base_path, cam2, cam2_0, "", self.extra_output
+    
     def __next__(self):
-        #if it's a ros node the output will also include the simulation reset signal
-        if isinstance(self.sources[0], image_converter):
-            lock_success = self.lock_extra.acquire(timeout=self.time_out)
-            if not lock_success:
-                raise StopIteration
-        self.count += 1
-        if not all(x.is_alive() for x in self.threads) or cv2.waitKey(1) == ord('q'):  # q to quit
-            cv2.destroyAllWindows()
+        """Return the next sequence."""
+        # Get the data from the next index
+        data = self.__getitem__(self.index)
+
+        # Increment the index and loop if necessary
+        self.index += 1
+        if self.index >= len(self):
             raise StopIteration
-        im0 = self.imgs.copy()
 
-
-
-        if self.transforms:
-            im = np.stack([self.transforms(x) for x in im0])  # transforms
-        else:
-            im = np.stack([LetterBox(self.imgsz, self.auto, stride=self.stride)(image=x) for x in im0])
-            im = im[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW
-            im = np.ascontiguousarray(im)  # contiguous
-
-        #if ros node, return the reset signal as well
-        if isinstance(self.sources[0], image_converter):
-
-            #do the same for the depth image queue
-            # self.depth_image = None
-            # tick = time.time()
-            # while self.sources[0].depth_image_queue.qsize() == 0 or self.sources[0].odom_queue.qsize() == 0:
-            #     # if no data received for time_out seconds, return None
-            #     if time.time() - tick > self.time_out:
-            #         raise StopIteration
-                
-            # if self.sources[0].depth_image_queue.qsize() > 0:
-            #     while(self.sources[0].depth_image_queue.qsize() > 0):
-            depth_image = self.sources[0].depth_image_queue.get()
-            self.depth_image = depth_image
-
-            #do the same for the odom queue
-            self.odom = None
-            #sleep until the odom queue is not empty
-            
-            # while self.sources[0].odom_queue.qsize() == 0: pass #FIXME: this is a hack to make sure the odom queue is not empty (for logging)
-            # if self.sources[0].odom_queue.qsize() > 0:
-            #     while(self.sources[0].odom_queue.qsize() > 0):
-            odom = self.sources[0].odom_queue.get()
-            self.odom = odom
-            self.gt = []
-            # if self.sources[0].gt_queue.qsize() > 0:
-            #     while(self.sources[0].gt_queue.qsize() > 0):
-            gt = self.sources[0].gt_queue.get()
-            self.gt = gt
-            
-            #make a dictionary that has the reset signal and the depth image
-            self.extra_output = { "depth_image": self.depth_image, "odom": self.odom, "gt": self.gt}
-            self.lock.release()
-            return self.sources, im, im0, None,'', self.extra_output
-        else:
-            return self.sources, im, im0, None, ''
-
+        return data
+    
     def __len__(self):
-        return len(self.sources)  # 1E12 frames = 32 streams at 30 FPS for 30 years
+        """Return the number of frames loaded."""
+        return len(self.cam2_files)
+    
+
+if __name__ == "__main__":
+    dataset = KittiLoader("/home/apera/mhmd/kittiMOT/data_kittiMOT/training", "0", transform = None)
---
+++ scripts/kitti_loader_utils.py
@@ -0,0 +1,86 @@
+import numpy as np
+from collections import namedtuple
+
+def rotx(t):
+    """Rotation about the x-axis."""
+    c = np.cos(t)
+    s = np.sin(t)
+    return np.array([[1,  0,  0],
+                     [0,  c, -s],
+                     [0,  s,  c]])
+
+
+def roty(t):
+    """Rotation about the y-axis."""
+    c = np.cos(t)
+    s = np.sin(t)
+    return np.array([[c,  0,  s],
+                     [0,  1,  0],
+                     [-s, 0,  c]])
+
+
+def rotz(t):
+    """Rotation about the z-axis."""
+    c = np.cos(t)
+    s = np.sin(t)
+    return np.array([[c, -s,  0],
+                     [s,  c,  0],
+                     [0,  0,  1]])
+
+def pose_from_oxts_packet(packet, scale):
+    """Helper method to compute a SE(3) pose matrix from an OXTS packet.
+    """
+    er = 6378137.  # earth radius (approx.) in meters
+
+    # Use a Mercator projection to get the translation vector
+    tx = scale * packet.lon * np.pi * er / 180.
+    ty = scale * er * \
+        np.log(np.tan((90. + packet.lat) * np.pi / 360.))
+    tz = packet.alt
+    t = np.array([tx, ty, tz])
+
+    # Use the Euler angles to get the rotation matrix
+    Rx = rotx(packet.roll)
+    Ry = roty(packet.pitch)
+    Rz = rotz(packet.yaw)
+    R = Rz.dot(Ry.dot(Rx))
+
+    # Combine the translation and rotation into a homogeneous transform
+    return R, t
+
+def read_calib_file(filepath):
+    """Read in a calibration file and parse into a dictionary."""
+    data = {}
+
+    with open(filepath, 'r') as f:
+        for line in f.readlines():
+            if ':' not in line:
+                key, value = line.split(' ',1)
+            else:
+                key, value = line.split(':', 1)
+            # The only non-float values in these files are dates, which
+            # we don't care about anyway
+            try:
+                data[key] = np.array([float(x) for x in value.split()])
+            except ValueError:
+                pass
+
+    return data
+
+def transform_from_rot_trans(R, t):
+    """Transforation matrix from rotation matrix and translation vector."""
+    R = R.reshape(3, 3)
+    t = t.reshape(3, 1)
+    return np.vstack((np.hstack([R, t]), [0, 0, 0, 1]))
+
+OxtsData = namedtuple('OxtsData', 'packet, T_w_imu')
+
+OxtsPacket = namedtuple('OxtsPacket',
+                        'lat, lon, alt, ' +
+                        'roll, pitch, yaw, ' +
+                        'vn, ve, vf, vl, vu, ' +
+                        'ax, ay, az, af, al, au, ' +
+                        'wx, wy, wz, wf, wl, wu, ' +
+                        'pos_accuracy, vel_accuracy, ' +
+                        'navstat, numsats, ' +
+                        'posmode, velmode, orimode')
