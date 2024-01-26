c7214ff53f2e7d660e5eab16ea006ba9db5f1172
mhmd-j <mo.jani1378@gmail.com>
2024-01-25 16:22:43 -0800

    dets reader added to kitti data loader (permatracks)

--- scripts/kitti_loader.py
+++ scripts/kitti_loader.py
@@ -16,6 +16,60 @@ from ultralytics.yolo.data.augment import LetterBox
 import kitti_loader_utils as utils
 
 class KittiLoader:
+    """Load KittiMOT dataset
+    Each frame is returned as a tuple of (base_path, image, image_raw, label, extra_output)
+    extra_output is a dictionary of {"depth": depthmap: np.NDarray(H,W),
+                                     "velodyne": velo: np.NDarray(N,4), 
+                                     "oxt": oxt: namedtuple("OxtsData", ["packet", "T_w_imu"])
+                                     "dets": dets, 
+                                     "gt": None}
+    each oxt packet is a namedtuple of OxtsPacket(lat, lon, alt, roll, pitch, yaw,
+                                                     vn, ve, vf, vl, vu, 
+                                                     ax, ay, az, af, al, au, 
+                                                     wx, wy, wz, wf, wl, wu, 
+                                                     pos_accuracy, vel_accuracy, 
+                                                     navstat, numsats, posmode, velmode, orimode)
+    lat:   latitude of the oxts-unit (deg)
+    lon:   longitude of the oxts-unit (deg)
+    alt:   altitude of the oxts-unit (m)
+    roll:  roll angle (rad),    0 = level, positive = left side up,      range: -pi   .. +pi
+    pitch: pitch angle (rad),   0 = level, positive = front down,        range: -pi/2 .. +pi/2
+    yaw:   heading (rad),       0 = east,  positive = counter clockwise, range: -pi   .. +pi
+    vn:    velocity towards north (m/s)
+    ve:    velocity towards east (m/s)
+    vf:    forward velocity, i.e. parallel to earth-surface (m/s)
+    vl:    leftward velocity, i.e. parallel to earth-surface (m/s)
+    vu:    upward velocity, i.e. perpendicular to earth-surface (m/s)
+    ax:    acceleration in x, i.e. in direction of vehicle front (m/s^2)
+    ay:    acceleration in y, i.e. in direction of vehicle left (m/s^2)
+    ay:    acceleration in z, i.e. in direction of vehicle top (m/s^2)
+    af:    forward acceleration (m/s^2)
+    al:    leftward acceleration (m/s^2)
+    au:    upward acceleration (m/s^2)
+    wx:    angular rate around x (rad/s)
+    wy:    angular rate around y (rad/s)
+    wz:    angular rate around z (rad/s)
+    wf:    angular rate around forward axis (rad/s)
+    wl:    angular rate around leftward axis (rad/s)
+    wu:    angular rate around upward axis (rad/s)
+    pos_accuracy:  velocity accuracy (north/east in m)
+    vel_accuracy:  velocity accuracy (north/east in m/s)
+    navstat:       navigation status (see navstat_to_string)
+    numsats:       number of satellites tracked by primary GPS receiver
+    posmode:       position mode of primary GPS receiver (see gps_mode_to_string)
+    velmode:       velocity mode of primary GPS receiver (see gps_mode_to_string)
+    orimode:       orientation mode of primary GPS receiver (see gps_mode_to_string)
+
+
+    Args:
+        kitii_base_path: path to kittiMOT dataset
+        sequence: sequence number
+        imgsz: image size
+        stride: stride for letterbox
+        auto: auto letterbox
+        transforms: image transformations
+
+"""
     def __init__(self, kitii_base_path: str, sequence: str, imgsz=640, stride=32, auto=True, transforms=None):
         self.base_path = kitii_base_path
         self.sequence = sequence
@@ -29,6 +83,7 @@ class KittiLoader:
         self._get_file_lists()
         self._load_calib()
         self._load_oxts() # it loads all the oxts data for a sequence. not an effiecient way to load oxts data
+        self._load_dets() # it loads all the det data for a sequence. not an effiecient way to load dets data
 
 
     def _get_file_lists(self):
@@ -53,6 +108,12 @@ class KittiLoader:
             os.path.join(self.base_path,
                         'oxts',
                          f'{self.sequence}.txt')))
+        
+        self.dets_files = sorted(glob.glob(
+            os.path.join(self.base_path,
+                        'permatrack_kitti_test',
+                        f'{self.sequence}.txt')))
+        
     def _load_calib(self):
         """Load and compute intrinsic and extrinsic calibration parameters."""
         # We'll build the calibration parameters as a dictionary, then
@@ -139,20 +200,49 @@ class KittiLoader:
                     oxts.append(utils.OxtsData(packet, T_w_imu))
 
         self.oxts = oxts
+
+    def _load_dets(self):
+        """Load detections from file.
+            method is taken from
+            https://github.com/noahcao/OC_SORT/blob/7a390a5f35dbbb45df6cd584588c216aea527248/tools/run_ocsort_public.py#L101
+
+        """
+        for seq_file in self.dets_files:
+            cats = ['Pedestrian', 'Car', 'Cyclist', "Van", "Truck"]
+            cat_ids = {cat: i for i, cat in enumerate(cats)}
+
+            print("starting seq {}".format(self.sequence))
+            seq_trks = np.empty((0, 18))
+            seq_file = open(seq_file)
+            lines = seq_file.readlines()
+            line_count = 0 
+            for line in lines:
+                print("{}/{}".format(line_count,len(lines)))
+                line_count+=1
+                line = line.strip()
+                tmps = line.strip().split()
+                tmps[2] = cat_ids[tmps[2]]
+                trk = np.array([float(d) for d in tmps])
+                trk = np.expand_dims(trk, axis=0)
+                seq_trks = np.concatenate([seq_trks, trk], axis=0)
+        self.seq_trks = seq_trks
+                
             
-    def __getitem__(self, index):
-        """Return the data from a particular index."""
+    def __getitem__(self, frame_index):
+        """Return the data from a particular frame_index."""
         # Load the data from disk
-        cam2_0 = cv2.imread(self.cam2_files[index].strip())
-        velo = np.fromfile(self.velo_files[index].strip(), dtype=np.float32).reshape((-1, 4))
+        cam2_0 = cv2.imread(self.cam2_files[frame_index].strip())
+        velo = np.fromfile(self.velo_files[frame_index].strip(), dtype=np.float32).reshape((-1, 4))
         velo[:, 3] = 1.
         upsampled_params = {"filtering": 1, "upsample": 1}
         intr_raw = np.hstack((self.calib.K_cam2, np.zeros((3, 1))))
         depthmap = utils.generate_depth(velodata=velo, M_velo2cam=self.calib.Tr_velo_cam, 
                                         width=cam2_0.shape[1], height=cam2_0.shape[0],
                                         intr_raw=intr_raw, params=upsampled_params)
-        oxt = self.oxts[index]
-
+        oxt = self.oxts[frame_index]
+        _det_ind = list(range(6,10)) + [-2, -1]
+        # Assuming that the frame index starts with 0 in detection file
+        dets = self.seq_trks[np.where(self.seq_trks[:,0]==frame_index)][:,_det_ind]
         # Apply the data transformations
         if self.transforms is not None:
             cam2 = self.transforms(cam2)
@@ -161,7 +251,7 @@ class KittiLoader:
             cam2 = cam2_0.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
             cam2 = np.ascontiguousarray(cam2_0)  # contiguous
         
-        self.extra_output = {"depth": depthmap, "velodyne": velo, "oxt": oxt, "gt": None} #TODO: add gt
+        self.extra_output = {"depth": depthmap, "velodyne": velo, "oxt": oxt, "dets": dets, "gt": None} #TODO: add gt
 
         return self.base_path, cam2, cam2_0, "", self.extra_output
     
@@ -184,7 +274,9 @@ class KittiLoader:
 
 if __name__ == "__main__":
     import time
-    dataset = KittiLoader("/home/apera/mhmd/kittiMOT/data_kittiMOT/training", "0001", transforms = None)
+    dataset = KittiLoader("/home/apera/mhmd/kittiMOT/data_kittiMOT/testing", "0001", transforms = None)
+    print(len(dataset))
+    print(dataset[-1])
     for data in dataset:
         print(data[0])
         cv2.imshow("image", data[1])
@@ -195,6 +287,7 @@ if __name__ == "__main__":
         print(data[4]["oxt"].packet)
         print(data[4]["oxt"].T_w_imu)
         print(data[4]["depth"].shape)
-        time.sleep(0.1)
+        print(data[4]["dets"])
+        # time.sleep(0.1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
