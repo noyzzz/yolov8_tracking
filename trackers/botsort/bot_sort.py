0f2fffd977d0996be4c26ca8b2f91a4dc497d22e
noyzzz <navidmahdian@uvic.ca>
2024-03-11 11:52:53 -0700

    bot sort turned off gmc

--- trackers/botsort/bot_sort.py
+++ trackers/botsort/bot_sort.py
@@ -386,9 +386,9 @@ class BoTSORT(MATracker):
         STrack.multi_predict(strack_pool)
 
         # Fix camera motion
-        warp = self.gmc.apply(img, dets)
-        STrack.multi_gmc(strack_pool, warp)
-        STrack.multi_gmc(unconfirmed, warp)
+        # warp = self.gmc.apply(img, dets)
+        # STrack.multi_gmc(strack_pool, warp)
+        # STrack.multi_gmc(unconfirmed, warp)
 
         # Associate with high score detection boxes
         raw_emb_dists = matching.embedding_distance(strack_pool, detections)
